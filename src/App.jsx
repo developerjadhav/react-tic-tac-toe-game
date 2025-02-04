d21a87863f597ba8f599120cc675639c54dea710
Abhishek <aajadhav638@gmail.com>
2025-02-04 22:47:53 +0530

    Updated CSS for responsiveness and fixed the player name related issue

--- src/App.jsx
+++ src/App.jsx
@@ -41,29 +41,30 @@ function deriveGameBoard(gameTurns) {
 
 function deriveWinner(gameBoard, players) {
   let winner;
-
+  
   for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
     const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
     const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
-
+    
     if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
       winner = players[firstSquareSymbol];
     }
   }
-
+  
   return winner;
 }
 
 function App() {
 
-  const [players, setPlayers] = useState({PLAYERS});
+  const [players, setPlayers] = useState(PLAYERS);
 
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
   const gameBoard = deriveGameBoard(gameTurns);
   const winner = deriveWinner(gameBoard, players);
+
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -83,7 +84,7 @@ function App() {
   function handleRestart() {
     setGameTurns([]);
   }
-
+  
   function handlePlayerNameChange(symbol, newName) {
     setPlayers(prevplayers => {
       return {
